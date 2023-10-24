77bf7c1a17ee44aef8a0d602675e0707b596c0b0
polievicia <85825175+Vicia0@users.noreply.github.com>
2023-10-24 19:10:18 +0200

    king was here

--- king.swift
+++ king.swift
@@ -1,1 +1,24 @@
-king
+import SwiftUI
+import RealityKit
+import ARKit
+
+struct BodyTrackingView: View {
+    @State private var bodyAnchor: ARBodyAnchor?
+
+    var body: some View {
+        ARViewContainer(bodyAnchor: bodyAnchor) { view in
+            // Add your custom body tracking content here
+        }
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    @Binding var bodyAnchor: ARBodyAnchor?
+
+    func makeUIView(context: Context) -> ARView {
+        let arView = ARView()
+        arView.session.delegate = context.coordinator
+        return arView
+        
+
+        //king was here
